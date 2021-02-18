4a12aaf637f7dbb0e1b25978aa6de324752d3522
Dominik Piotrowicz <dom.piotrowicz@gmail.com>
2021-02-18 17:54:26 +0100

    Change Font, visual fixes

--- react/src/components/Calendar/WeekDays.js
+++ react/src/components/Calendar/WeekDays.js
@@ -1,32 +1,42 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const WeekDays = () => {
-    const container = {
-        hidden: { opacity: 0 },
-        show: {
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.05
-            }
-        }
-    }
-    const item = {
-        hidden: { opacity: 0, x: -25 },
-        show: { opacity: 1, x: 0 }
-    }
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.05,
+      },
+    },
+  };
+  const item = {
+    hidden: { opacity: 0, x: -25 },
+    show: { opacity: 1, x: 0 },
+  };
 
-    const calendarDays = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela']
-        .map(day => {
-            return <motion.li key={day} className="calendar__names-item" variants={item}>{day}</motion.li>
-        });
+  const calendarDays = ["pon.", "wt.", "śr.", "czw.", "pt.", "sb.", "ndz."].map(
+    (day) => {
+      return (
+        <motion.li key={day} className="calendar__names-item" variants={item}>
+          {day}
+        </motion.li>
+      );
+    }
+  );
 
-    return (
-        <div className="calendar__names">
-            <motion.ul className="calendar__names-list" variants={container} initial="hidden" animate="show">
-                {calendarDays}
-            </motion.ul>
-        </div>
-    )
-}
+  return (
+    <div className="calendar__names">
+      <motion.ul
+        className="calendar__names-list"
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
+        {calendarDays}
+      </motion.ul>
+    </div>
+  );
+};
 
 export default WeekDays;
